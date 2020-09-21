6f4214ab5ecf4239408912ca0017da9e05fb01ad
Junior <jjalves96@gmail.com>
2020-09-21 08:06:19 -0300

    config: Add RTL and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />);
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument();
+
+    // Snapshot
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
