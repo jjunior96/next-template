72fc6cdb080db9fa43faa8523aa25bc591cf650b
Junior <jjalves96@gmail.com>
2020-09-22 08:17:26 -0300

    config: GlobalStyles with Styled Components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shorcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="description" content="Boilerplate TS, NextJS" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
+}
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-type Props = {
-  title: string;
-};
+import Main from 'components/Main';
 
-export default function Home({ title = 'React Avançado' }: Props) {
-  return <h1>{title}</h1>;
+export default function Home() {
+  return <Main />;
 }
