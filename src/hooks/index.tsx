145abaf07b692fced5ff0798e25526098c4e6780
Junior Alves <jjalves96@gmail.com>
2021-02-03 15:56:22 -0300

    chore: add hooks folder

---
+++ src/hooks/index.tsx
@@ -0,0 +1,10 @@
+// import { AuthProvider } from './auth';
+// import { ToastProvider } from './toast';
+
+const AppProvider: React.FC = ({ children }) => (
+  // <AuthProvider>
+  // <ToastProvider>{children}</ToastProvider>
+  // </AuthProvider>
+);
+
+export default AppProvider;
