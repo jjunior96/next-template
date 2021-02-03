586346d485906b8a1ee495689914ffd410b7944f
Junior Alves <jjalves96@gmail.com>
2021-02-03 15:57:45 -0300

    chore: add new infos into head page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import GlobalStyles from 'styles/global';
+import GlobalStyles from 'styles/globals';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boilerplate</title>
+        <title>App Name</title>
         <link rel="shorcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <meta name="description" content="Boilerplate TS, NextJS" />
+        <meta name="description" content="App Name" />
       </Head>
+        <Component {...pageProps} />
       <GlobalStyles />
-      <Component {...pageProps} />
     </>
   );
 }
