9279f4431247f3e06ad1915134a932f30c337760
Junior Alves <jjalves96@gmail.com>
2021-02-03 15:56:45 -0300

    feat: add new global style

---
+++ src/styles/globals.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  html, body, #__next {
+    height: 100%;
+  }
+  body {
+    font-family: Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: #0c0f16;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
