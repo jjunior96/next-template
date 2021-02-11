74ad15a99a57c9ce36a7448db6c34cfaae51e30b
Junior Alves <jjalves96@gmail.com>
2021-02-11 15:36:23 -0300

    chore: import styles example

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image';
 
+import { Container, Content } from './styles';
+
 const Main = ({ title = 'App Name' }) => (
-  <main>
-    <h1>{title}</h1>
-    <Image src="/assets/vercel.svg" width={100} height={100} />
-  </main>
+  <Container>
+    <Content>
+      <h1>{title}</h1>
+      <Image
+        src="/assets/vercel.svg"
+        width={100}
+        height={100}
+        alt="Logo da Vercel, empresa criadora do Next"
+      />
+    </Content>
+  </Container>
 );
 
 export default Main;
