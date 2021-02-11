58cd785535fc3c29b46f6e966d30e78459afbd3e
Junior Alves <jjalves96@gmail.com>
2021-02-11 15:35:46 -0300

    feat: create styles example

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  margin: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    color: #fff;
+  }
+`;
