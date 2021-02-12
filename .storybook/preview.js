6d2a2b68182ad58788caad6e83c9168c3426b99b
Junior Alves <jjalves96@gmail.com>
2021-02-12 14:45:23 -0300

    chore: config ThemeProvider from styled-components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../src/styles/global';
+import theme from '../src/styles/theme';
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
 
