fdcec246f77720f9d17cadea4dd959a22ca5af98
Junior Alves <jjalves96@gmail.com>
2021-02-12 14:42:53 -0300

    chore: config ThemeProvider from styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>App Name</title>
         <link rel="shorcut icon" href="/img/bg.png" />
@@ -15,7 +17,7 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 }
 
