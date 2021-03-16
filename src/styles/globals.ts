74048b489626e031a231dd8032f7297a97670d39
Junior Alves <jjalves96@gmail.com>
2021-03-16 18:48:49 -0300

    chore: add new styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -7,16 +7,20 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     outline: none;
   }
+
   html {
     font-size: 62.5%;
   }
+
   html, body, #__next {
     height: 100%;
   }
-  body {
+
+  body, input, textarea, button {
     font-family: Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     background-color: #0c0f16;
   }
+
   button {
     cursor: pointer;
   }
