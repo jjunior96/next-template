e0c75a5973bf8abd315a7f13b1b81e04ec19813b
Junior Alves <jjalves96@gmail.com>
2021-11-02 18:37:20 -0300

    fix: fix nextconfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withPWA = require('next-pwa');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
+  swcMinify: true,
+  experimental: {
+    // urlImports: ['https://cdn.skypack.dev']
+  },
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
