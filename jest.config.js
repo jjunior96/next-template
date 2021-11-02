ea3f721b9ca1e6ef2b3e66ae48c2a1c53348c149
Junior Alves <jjalves96@gmail.com>
2021-11-02 18:37:11 -0300

    fix: fix jestconfig

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  // extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  // transform: {
+  //   '^.+\\.(t|j)sx?$': ['@swc/jest']
+  // }
 };
